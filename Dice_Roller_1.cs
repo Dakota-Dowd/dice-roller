7c61c92f236286017654152de6379c1d9c88e3a8
Dakota <Jmdowd2020@gmail.com>
2026-01-16 22:55:35 -0700

    dice roller

---
+++ Dice_Roller_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_2_Assignment
+{
+    internal class DiceRoller
+    {
+        private Random rnd = new Random();
+
+        // Returns an array where index is the sum(2-12)
+        public int[] RollDice(int numRolls)
+        {
+            int[] counts = new int[13];
+            for (int i = 0; i < numRolls; i++)
+            {
+                int die1 = rnd.Next(1, 7);
+                int die2 = rnd.Next(1, 7);
+                int sum = die1 + die2;
+                counts[sum]++;
+            }
+            return counts;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_2_Assignment 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolsl would you like to simulate? ");
+            int numRolls = int.Parse(Console.ReadLine() ?? "0");
+
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDice(numRolls);
+
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {numRolls}.");
+
+            for (int sum = 2; sum <= 12; sum++)
+            {
+                int percent = (int)Math.Round(results[sum] * 100.00 / numRolls);
+                Console.Write($"{sum}: ");
+                for (int i = 0; i < percent; i++) // Loop through the stars
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
+
